01687f7569a5e5c7f51dd86959893383ed6940ca
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-17 19:51:56 +0800

    add overview to lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,40 @@
 //! # stp258 Module
+//!
+//! ## Overview
+//!
+//! The stp258 module provides a mixed stablecoin system, by configuring a
+//! native currency which implements `ExtendedBasicCurrency`, and a
+//! multi-currency which implements `SettCurrency`.
+//!
+//! It also provides an adapter, to adapt `frame_support::traits::Currency`
+//! implementations into `ExtendedBasicCurrency`.
+//!
+//! The stp258 module provides functionality of both `ExtendedSettCurrency`
+//! and `ExtendedBasicCurrency`, via unified interfaces, and all calls would be
+//! delegated to the underlying multi-currency and base currency system.
+//! A native currency ID could be set by `Config::GetNativeCurrencyId`, to
+//! identify the native currency.
+//!
+//! ### Implementations
+//!
+//! The stp258 module provides implementations for following traits.
+//!
+//! - `SettCurrency` - Abstraction over a fungible multi-currency stablecoin system.
+//! - `ExtendedSettCurrency` - Extended `SettCurrency` with additional helper
+//!   types and methods, like updating balance
+//! by a given signed integer amount.
+//!
+//! ## Interface
+//!
+//! ### Dispatchable Functions
+//!
+//! - `transfer` - Transfer some balance to another account, in a given
+//!   currency.
+//! - `transfer_native_currency` - Transfer some balance to another account, in
+//!   native currency set in
+//! `Config::NativeCurrency`.
+//! - `update_balance` - Update balance by signed integer amount, in a given
+//!   currency, root origin required.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
