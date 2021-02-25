a30d079b6b0a1e3c3a6b6b9594a2321bf5790ef0
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-25 08:44:26 +0800

    Remove weights

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,6 @@ use frame_support::{
 		Currency as PalletCurrency, ExistenceRequirement, Get, LockableCurrency as PalletLockableCurrency,
 		ReservableCurrency as PalletReservableCurrency, WithdrawReasons,
 	},
-	weights::Weight,
 };
 use frame_system::{ensure_root, ensure_signed, pallet_prelude::*};
 
