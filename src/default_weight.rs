e6b872de69b4d561bbdea17dd853489739fb84d4
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-02 23:55:35 +0800

    TOKENS TO CURRENCIES

--- src/default_weight.rs
+++ src/default_weight.rs
@@ -7,14 +7,23 @@
 use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 impl crate::WeightInfo for () {
-	fn transfer() -> Weight {
-		(158_835_000 as Weight)
-			.saturating_add(DbWeight::get().reads(4 as Weight))
+	fn transfer_non_native_currency() -> Weight {
+		(172_011_000 as Weight)
+			.saturating_add(DbWeight::get().reads(5 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
-	fn transfer_all() -> Weight {
-		(162_545_000 as Weight)
-			.saturating_add(DbWeight::get().reads(4 as Weight))
+	fn transfer_native_currency() -> Weight {
+		(43_023_000 as Weight)
+	}
+	fn update_balance_non_native_currency() -> Weight {
+		(137_440_000 as Weight)
+			.saturating_add(DbWeight::get().reads(5 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
+	}
+	fn update_balance_native_currency_creating() -> Weight {
+		(64_432_000 as Weight)
+	}
+	fn update_balance_native_currency_killing() -> Weight {
+		(62_595_000 as Weight)
 	}
 }
